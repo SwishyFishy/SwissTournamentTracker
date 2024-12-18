0b2613675dadf98f678e0215c0190c1d1f4da2aa
Jonah Galloway-Fenwick <jonahgallowayfenwick@trentu.ca>
2024-12-18 14:43:05 -0500

    Start running server on localhost tests

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 
-const port = 8080;
+const port = 1234;
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 })
+
+// Include ip address in listen - does it work on not Trent network?
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
