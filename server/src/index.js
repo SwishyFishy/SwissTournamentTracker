de516a17488b764d2d67d6d93f394cf3bad16399
SwishyFishy <jonahcoyotegf@icloud.com>
2024-12-19 14:27:29 -0500

    Send response on submit form

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,24 @@
+// Get express
 const express = require("express");
 const app = express();
 
+// Get required modules
 const {ipv4, port} = require("./private.js");
+const path = require("path");
 
+// Define location of static files
+app.use(express.static(__dirname + 'public'));
+
+// Define responses
 app.get("/", (req, res) => {
-    res.send("Hello Again,  World!");
+    res.sendFile(path.join(__dirname, "public", "test.html"));
 })
 
+app.get("/submit", (req, res) => {
+    res.send("Submitted!");
+})
+
+// Listen on port
 app.listen(port, ipv4, () => {
     console.log(`Server started on port ${port}`);
     console.log(`${ipv4}:${port}`);
