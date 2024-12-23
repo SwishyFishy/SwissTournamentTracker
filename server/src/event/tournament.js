a3049b22def701860044305f885f95ee47b28871
SwishyFishy <jonahcoyotegf@icloud.com>
2024-12-23 09:11:07 -0500

    Randomize player order, optimize player addition to participants array, add 2d array matches tracker

--- server/src/event/tournament.js
+++ server/src/event/tournament.js
@@ -3,7 +3,12 @@ class Tournament
     // Constructor
     constructor(participants = [])
     {
-        this.participants = participants;
+        this.participants = [];
+        participants.forEach((player) => {
+            this.participants.push(player);
+        })
+
+        this.matches = []
         this.rounds = 0;
         this.currentRount = 0;
         this.hasStarted = false;
@@ -14,6 +19,24 @@ class Tournament
 
     StartEvent()
     {
+        // Shuffle the participants array so that join-order is sure to not matter
+        for (let i = this.participants.length - 1; i >= 0; i--) 
+        {
+            const index = Math.floor(Math.random() * (i + 1));
+            [this.participants[i], this.participants[index]] = [this.participants[index], this.participants[i]];
+        }
+
+        // Initialize a 2d array of booleans to false to track which players have played a match
+        // Only initialie the lower triangle, since the upper triangle is redundant. Use the diagonal for byes
+        for (let i = 0; i < this.participants.length; i++)
+        {
+            this.matches[i].push([]);
+            for (let j = 0; j <= i; j++)
+            {
+                this.matches[i].push(false);
+            }
+        }
+
         this.rounds = Math.max(Math.ceil(Math.log2(participants.length)), 3);
         this.currentRount = 1;
         this.hasStarted = true;
