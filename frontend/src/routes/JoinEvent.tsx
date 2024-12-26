f5659088dcebf3c93c35a413827f29dad9193cd2
SwishyFishy <jonahcoyotegf@icloud.com>
2024-12-26 15:09:41 -0500

    Stated JoinEvent player name entry

--- frontend/src/routes/JoinEvent.tsx
+++ frontend/src/routes/JoinEvent.tsx
@@ -1,7 +1,21 @@
+
+import { useState } from "react";
+
 function JoinEvent(): JSX.Element
 {
+    const [name, setName] = useState("");
+
+    const handleTextInput = (e: any) => {
+        setName(e.currentTarget?.value);
+    };
+
     return(
-        <p>JoinEvent page</p>
+        <div className="joinEvent">
+            <h1>Join Event</h1>
+            <form>
+                <input type="text" name="name" id="name" placeholder="Your Name" value={name} onChange={handleTextInput}/>
+            </form>
+        </div>
     );
 }
 
