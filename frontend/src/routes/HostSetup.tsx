6761c0f7f758360921a9ccdd8c47f8edf10c9e40
SwishyFishy <jonahcoyotegf@icloud.com>
2024-12-26 20:54:09 -0500

    Host can kick players from event creating page

--- frontend/src/routes/HostSetup.tsx
+++ frontend/src/routes/HostSetup.tsx
@@ -7,6 +7,14 @@ function HostSetup(): JSX.Element
     const [players, setPlayers] = useState<Array<{id: string, name: string}>>([{id: '1', name: 'jonah'}]);
     const serverUrl: string = useContext(CONTEXT_serverBaseUrl);
 
+    // Kick a player using the X button
+    const handleKickPlayer = (e: any) => {
+        const kickId: string = e.currentTarget.parentElement.getAttribute('data-id');
+        setPlayers((curPlayers) => {
+            return curPlayers.filter((p) => p.id != kickId)
+        });
+    }
+
     useEffect(() => {
         // Create a handler to recieve pushed server events and append players to the player list
         const playerJoinEventSource: EventSource = new EventSource(serverUrl);
@@ -23,11 +31,11 @@ function HostSetup(): JSX.Element
         <div className="eventAdminHome">
             <h1>Event Lobby</h1>
             <h2>Players</h2>
-            <ol>
+            <ul>
                 {players.map((player) => (
-                    <li key={player.id}>{player.name} <input type="button" name="kick" id="kick" value="X" /></li>
+                    <li key={player.id} data-id={player.id}>{player.name} <input type="button" name="kick" id="kick" value="X" onClick={handleKickPlayer} /></li>
                 ))}
-            </ol>
+            </ul>
             <form>
                 <input type="submit" name="submit" id="submit" value="Start Event" />
             </form>
