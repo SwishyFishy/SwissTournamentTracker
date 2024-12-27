5f854509a8ed9b0767315a073e82709ed9d59921
SwishyFishy <jonahcoyotegf@icloud.com>
2024-12-26 21:08:08 -0500

    Tidy up CSS - remove redundancy after standardization

--- frontend/src/routes/ErrorPage.tsx
+++ frontend/src/routes/ErrorPage.tsx
@@ -3,7 +3,7 @@ import '../styles/ErrorPage.css';
 function ErrorPage(): JSX.Element
 {
     return(
-        <div className="errorPage">
+        <div className="wrapper errorPage">
             <h1>Uh Oh...</h1>
             <p>Something went wrong. Please contact the event coordinator for assistance.</p>
         </div>
