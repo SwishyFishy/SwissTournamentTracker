7dc4a0cf4bfeb2113f4983603836f0fb1e64258d
SwishyFishy <jonahcoyotegf@icloud.com>
2024-12-26 19:07:02 -0500

    Stated Event code entry

--- frontend/src/routes/JoinEvent.tsx
+++ frontend/src/routes/JoinEvent.tsx
@@ -4,16 +4,22 @@ import { useState } from "react";
 function JoinEvent(): JSX.Element
 {
     const [name, setName] = useState("");
+    const [code, setCode] = useState("");
 
-    const handleTextInput = (e: any) => {
+    const handleNameInput = (e: any) => {
         setName(e.currentTarget?.value);
+    };
+    
+    const handleCodeInput = (e: any) => {
+        setCode(e.currentTarget?.value);
     };
 
     return(
         <div className="joinEvent">
             <h1>Join Event</h1>
             <form>
-                <input type="text" name="name" id="name" placeholder="Your Name" value={name} onChange={handleTextInput}/>
+                <input type="text" name="name" id="name" placeholder="Your Name" value={name} onChange={handleNameInput}/>
+                <input type="text" name="code" id="code" placeholder="Event Code" value={code} onChange={handleCodeInput}/>
             </form>
         </div>
     );
