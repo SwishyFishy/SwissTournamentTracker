3120b6f3665d601c244c9a2a341b360530aa193a
SwishyFishy <jonahcoyotegf@icloud.com>
2024-12-26 21:37:41 -0500

    HostSetup styling

--- frontend/src/routes/HostSetup.tsx
+++ frontend/src/routes/HostSetup.tsx
@@ -2,9 +2,21 @@ import { useState, useEffect, useContext } from "react";
 
 import { CONTEXT_serverBaseUrl } from "../main";
 
+import '../styles/HostSetup.css';
+
 function HostSetup(): JSX.Element
 {
-    const [players, setPlayers] = useState<Array<{id: string, name: string}>>([]);
+    const [players, setPlayers] = useState<Array<{id: string, name: string}>>([
+        {id: '1', name: 'jonah'},
+        {id: '2', name: 'curt'},
+        {id: '3', name: 'gregory'},
+        {id: '4', name: 'spock'},
+        {id: '5', name: 'aslan'},
+        {id: '6', name: 'janice'},
+        {id: '7', name: 'hela'},
+        {id: '8', name: 'ken'},
+        {id: '9', name: 'chad'}
+    ]);
     const serverUrl: string = useContext(CONTEXT_serverBaseUrl);
 
     // Kick a player using the X button
@@ -28,12 +40,12 @@ function HostSetup(): JSX.Element
     }, []);
 
     return(
-        <div className="wrapper eventAdminHome">
+        <div className="wrapper hostSetup">
             <h1>Event Lobby</h1>
             <h2>Players</h2>
             <ul>
                 {players.map((player) => (
-                    <li key={player.id} data-id={player.id}>{player.name} <input type="button" name="kick" id="kick" value="X" onClick={handleKickPlayer} /></li>
+                    <li key={player.id} data-id={player.id}><span>{player.name}</span> <input type="button" name="kick" id="kick" value="X" onClick={handleKickPlayer} /></li>
                 ))}
             </ul>
             <form>
