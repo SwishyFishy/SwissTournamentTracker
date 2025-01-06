b0b5e3055d69a8544f1bc349dfdbdc61fc6acaf8
Jonah Galloway-Fenwick <jonahgallowayfenwick@trentu.ca>
2025-01-06 13:19:57 -0500

    Leaderboard display

--- frontend/src/routes/Leaderboard.tsx
+++ frontend/src/routes/Leaderboard.tsx
@@ -5,6 +5,8 @@ import { PlayerStats } from "../types";
 
 import { CONTEXT_serverBaseUrl } from "../main";
 
+import '../styles/Leaderboard.css';
+
 function Leaderboard() 
 {
     const [results, setResults] = useState<Array<PlayerStats>>([]);    
@@ -25,12 +27,25 @@ function Leaderboard()
     }, [])
 
     return(
-        <div className="wrapper leaderboardHome">
+        <div className="wrapper leaderboard">
             <h1>Results: </h1>
             <ul>
+                <li className="headerRow">
+                    <span>Player</span>
+                    <span>Pts</span>
+                    <span>Record</span>
+                    <span>OMW</span>
+                    <span>GW</span>
+                    <span>OGW</span>
+                </li>
                 {results.map((result) => ( 
                     <li key={result.id}>
-                        
+                        <span>{result.name}</span>
+                        <span>{result.points}</span>
+                        <span>{result.wins} - {result.losses} - {result.draws}</span>
+                        <span>{result.omw.toFixed(3)}</span>
+                        <span>{result.gw.toFixed(3)}</span>
+                        <span>{result.ogw.toFixed(3)}</span>
                     </li>
                 ))}
             </ul>
