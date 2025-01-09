1eb40e3c4629bcfe9eba2dce945b9e5d299ea1f0
Jonah Galloway-Fenwick <jonahcoyotegf@icloud.com>
2025-01-08 23:02:33 -0500

    relevant imports to connect to director endpoint - excuse to test git configs

--- frontend/src/routes/Layout.tsx
+++ frontend/src/routes/Layout.tsx
@@ -1,14 +1,22 @@
+import { useEffect, useContext, createContext } from "react";
 import { Outlet } from "react-router";
 
+import { Match } from "../types";
+import { CONTEXT_serverBaseUrl } from "../main";
+
 import '../styles/Layout.css';
+
+export const CONTEXT_currentMatches = createContext([]);
 
 function Layout(): JSX.Element
 {
     return(
-        <div className="layout">
-            <Outlet />
-            <footer>&copy; 2025 Jonah Galloway-Fenwick</footer>
-        </div>
+        <CONTEXT_currentMatches.Provider value={[]}>
+            <div className="layout">
+                <Outlet />
+                <footer>&copy; 2025 Jonah Galloway-Fenwick</footer>
+            </div>
+        </CONTEXT_currentMatches.Provider>
     );
 }
 
