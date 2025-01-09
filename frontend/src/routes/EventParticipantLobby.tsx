419c38d743a38bb35b7176b85c2642bf61b41a87
Jonah Galloway-Fenwick <jonahcoyotegf@icloud.com>
2025-01-09 12:02:37 -0500

    Add connection to server which navigates to match page when tournament starts

--- frontend/src/routes/EventParticipantLobby.tsx
+++ frontend/src/routes/EventParticipantLobby.tsx
@@ -1,5 +1,21 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+
+import CreateConnection from "../functions/server_liaison";
+import { SubscribedData } from "../types";
+
 function EventParticipantLobby(): JSX.Element
 {
+    const navigate = useNavigate();
+
+    // Invoke server_liaison to connect on load
+    useEffect(() => CreateConnection((data: SubscribedData) => {
+        if (data.status == "running")
+        {
+            navigate("/event/pairing");
+        }
+    }), []);
+
     return(
         <div className="wrapper eventParticipantHome">
             <p>EventParticipantLobby page</p>
