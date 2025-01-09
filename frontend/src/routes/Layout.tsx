df6715e6175f9a7d1ddafd302000c6a8c31c49f5
Jonah Galloway-Fenwick <jonahcoyotegf@icloud.com>
2025-01-09 08:45:19 -0500

    Change approach to problem, revert

--- frontend/src/routes/Layout.tsx
+++ frontend/src/routes/Layout.tsx
@@ -1,8 +1,5 @@
-import { useEffect, useContext, createContext } from "react";
+import { createContext } from "react";
 import { Outlet } from "react-router";
-
-import { Match } from "../types";
-import { CONTEXT_serverBaseUrl } from "../main";
 
 import '../styles/Layout.css';
 
@@ -11,12 +8,10 @@ export const CONTEXT_currentMatches = createContext([]);
 function Layout(): JSX.Element
 {
     return(
-        <CONTEXT_currentMatches.Provider value={[]}>
-            <div className="layout">
-                <Outlet />
-                <footer>&copy; 2025 Jonah Galloway-Fenwick</footer>
-            </div>
-        </CONTEXT_currentMatches.Provider>
+        <div className="layout">
+            <Outlet />
+            <footer>&copy; 2025 Jonah Galloway-Fenwick</footer>
+        </div>
     );
 }
 
