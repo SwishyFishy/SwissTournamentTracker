6197b69f2a99701f5c6bea7f39e7c0c5e0dafc01
Jonah Galloway-Fenwick <jonahcoyotegf@icloud.com>
2025-01-10 10:55:09 -0500

    Instructional text

--- frontend/src/routes/EventParticipantLobby.tsx
+++ frontend/src/routes/EventParticipantLobby.tsx
@@ -21,8 +21,10 @@ function EventParticipantLobby(): JSX.Element
     }), []);
 
     return(
-        <div className="wrapper eventParticipantHome">
-            <p>EventParticipantLobby page</p>
+        <div className="wrapper eventParticipantLobby">
+            <h1>You're In!</h1>
+            <p>Please remain on this page until the event begins.</p>
+            <p>The event administrator will announce the names of all registered participants before the event begins. Please contact them immediately if your name is not read aloud.</p>
         </div>
     );
 }
