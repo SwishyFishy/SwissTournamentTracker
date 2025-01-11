7c94e67d89ed2006683862b712cbbf38b0575ebd
Jonah Galloway-Fenwick <jonahcoyotegf@icloud.com>
2025-01-11 13:12:37 -0500

    Remove unused context

--- frontend/src/routes/Layout.tsx
+++ frontend/src/routes/Layout.tsx
@@ -1,11 +1,8 @@
-import { createContext } from "react";
 import { Outlet } from "react-router";
 
 import Footer from "../components/Footer";
 
 import '../styles/Layout.css';
-
-export const CONTEXT_currentMatches = createContext([]);
 
 function Layout(): JSX.Element
 {
