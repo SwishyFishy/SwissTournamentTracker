9d6afbf3de9d97b1d1bb20d1fb6a474f21b8c4cd
SwishyFishy <jonahcoyotegf@icloud.com>
2025-01-14 23:07:16 -0500

    Reenable strict mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,5 @@
 import { createContext } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import routes from './routes/routes.tsx'
@@ -10,5 +11,7 @@ const router = createBrowserRouter(routes);
 export const CONTEXT_serverBaseUrl: React.Context<string> = createContext(`http://${server_ipv4}:${server_port}`);
 
 createRoot(document.getElementById('root')!).render(
-    <RouterProvider router={router} />
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
 )
