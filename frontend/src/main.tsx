d61cccfa1afb8e935864ef7d13c8d6a345fbd743
SwishyFishy <jonahcoyotegf@icloud.com>
2025-05-14 14:31:07 -0400

    Use Glitch url instead of ip+port

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,11 @@ import { createContext } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import routes from './routes/routes.tsx'
-import {server_ipv4, server_port} from './private.tsx';
 
 import './styles/index.css'
 
 const router = createBrowserRouter(routes);
-export const CONTEXT_serverBaseUrl: React.Context<string> = createContext(`http://${server_ipv4}:${server_port}`);
+export const CONTEXT_serverBaseUrl: React.Context<string> = createContext(`https://swisstournamenttrackerserver.glitch.me`);
 
 createRoot(document.getElementById('root')!).render(
     <RouterProvider router={router} />
